09fd9649ed1875a7c8da9e957548f368cf2f89f9
domingo79 <andria140779@yahoo.it>
2021-07-02 17:48:31 +0200

    dipartimenti

--- esercizio-b2/quary.sql
+++ esercizio-b2/quary.sql
@@ -37,6 +37,7 @@ LIKE "%magistrale%";
 
 -- 7.Da quanti dipartimenti è composta l'università?(12)
 SELECT COUNT(`id`) 
+AS dipartimenti_totali
 FROM `departments`;
 
 -- 8.Quanti sono gli insegnanti che non hanno un numero di telefono?(50)
