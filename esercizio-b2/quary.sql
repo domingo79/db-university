2db8630aae25e652a935b3f2614d052a0d0e1cb5
domingo79 <andria140779@yahoo.it>
2021-07-02 17:42:34 +0200

    studenti che hanno più di 30 anni 2quary 2res diff

--- esercizio-b2/quary.sql
+++ esercizio-b2/quary.sql
@@ -8,10 +8,14 @@ SELECT *
 FROM `courses` 
 WHERE `cfu`> 10;
 
--- 3.Selezionare tutti gli studenti che hanno più di 30anni
+-- 3.Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT *
 FROM `students`
-WHERE YEAR(date_of_birth) < YEAR(CURDATE()) -30;
+WHERE YEAR(date_of_birth) < YEAR(CURDATE()) -30;  --3186
+
+SELECT * 
+FROM `students` 
+WHERE TIMESTAMPDIFF(YEAR,`date_of_birth`,CURDATE()) -30; --4852
 
 -- 4.Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso dilaurea(286)
 SELECT *
